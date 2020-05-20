9a4e218801d2088a9f8b84c9943ef223166e47d7
Priyadarshan Vijay <Priyadarshanvijay@gmail.com>
2020-05-20 15:18:23 +0530

    Added More Routes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 require('dotenv').config()
 require('./db/mongoose');
 const userRouter = require('./routes/user');
+const authorRouter = require('./routes/author');
+const bookRouter = require('./routes/book');
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(userRouter);
+app.use(authorRouter);
+app.use(bookRouter);
+
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
--- src/models/user.js
+++ src/models/user.js
@@ -4,83 +4,83 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"');
-            }
-        }
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
     }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "password"');
+      }
+    }
+  }
 });
 
 userSchema.virtual('books', {
-    ref: 'Book',
-    localField: '_id',
-    foreignField: 'isuued_by'
+  ref: 'Book',
+  localField: '_id',
+  foreignField: 'issued_by'
 });
 
 userSchema.methods.toJSON = function () {
-    const user = this;
-    const userObject = user.toObject();
+  const user = this;
+  const userObject = user.toObject();
 
-    delete userObject.password;
+  delete userObject.password;
 
-    return userObject;
+  return userObject;
 };
 
 userSchema.methods.generateAuthToken = async function () {
-    const user = this;
-    const token = jwt.sign({ _id: user._id.toString() }, process.env.SECRET);
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.SECRET);
 
-    return token;
+  return token;
 };
 
 userSchema.statics.findByCredentials = async (email, password) => {
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (!user) {
-        throw new Error('Unable to login');
-    }
+  if (!user) {
+    throw new Error('Unable to login');
+  }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+  const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
-        throw new Error('Unable to login');
-    }
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
 
-    return user;
+  return user;
 };
 
 // Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
-    const user = this;
+  const user = this;
 
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8);
-    }
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
 
-    next();
+  next();
 });
 
 const User = mongoose.model('User', userSchema);

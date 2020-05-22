2cbf01ef6bca7fb3781dee0e1a41b9a4bbab2b75
Priyadarshan Vijay <Priyadarshanvijay@gmail.com>
2020-05-22 21:30:07 +0530

    Modified Home

--- client/src/home.js
+++ client/src/home.js
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {useHistory, withRouter} from 'react-router-dom';
-import { Menu, Container, Dropdown, Image } from 'semantic-ui-react';
+import { Menu, Container, Dropdown, Image, Segment } from 'semantic-ui-react';
 import withSidebar from './components/withSidebar';
 import withAuth from './components/auth_components/withAuth';
 
 const Home = (props) => {
   const history = useHistory();
+
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem('user')).name);
+  }, []);
   
   return (
-    <h1>LOl</h1>
+    <Segment>
+      <h1>Hello {user}, Welcome To EXAMPLE Library, please Choose One of the Options From The Left</h1>
+      <Segment>
+        Todays Date is {new Date().toLocaleDateString()}
+      </Segment>
+    </Segment>
   )
 }
 
